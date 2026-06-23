a4a96f222443b6a2a00da7ce2b3e67b70ad2cb94
Long Shihui <longshihui_1995@qq.com>
2026-06-23 09:13:29 +0800

    docs(repo): 补充开发和发版文档

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,43 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
     ignores: [
-      '**/node_modules/**',
-      '**/dist/**',
-      '**/coverage/**',
-      '**/.vitepress/cache/**',
-      '**/.vitepress/dist/**',
-      '**/pnpm-lock.yaml'
-    ]
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/coverage/**",
+      "**/.vitepress/.temp/**",
+      "**/.vitepress/cache/**",
+      "**/.vitepress/dist/**",
+      "**/pnpm-lock.yaml",
+    ],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    files: ['**/*.{js,mjs,cjs,ts}'],
+    files: ["**/*.{js,mjs,cjs,ts}"],
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+      ecmaVersion: "latest",
+      sourceType: "module",
       globals: {
         ...globals.browser,
         ...globals.node,
-        ...globals.es2022
-      }
-    }
+        ...globals.es2022,
+      },
+    },
   },
   {
-    files: ['**/*.ts'],
+    files: ["**/*.ts"],
     rules: {
-      'no-undef': 'off',
-      '@typescript-eslint/consistent-type-imports': [
-        'error',
+      "no-undef": "off",
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
         {
-          prefer: 'type-imports'
-        }
-      ]
-    }
-  }
-)
+          prefer: "type-imports",
+        },
+      ],
+    },
+  },
+);
